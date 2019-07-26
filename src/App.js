b5687192f4360a925b2557b7f9ce2ffd1eeae767
gregoriB <brandon.gregori@gmail.com>
2019-07-25 19:13:09 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import ReCAPTCHA from "react-google-recaptcha";
 import './App.css';
 
 function App() {
-  // const [isRecaptchaGood, setIsRecaptchaGood] = useState(false);
-  // const recaptchaRef = useRef(null);
-  const handleSubmit = e => {
-    e.preventDefault();
-    // console.log(recaptchaRef.current.getValue())
-  }
-
   return (
     <div className="App">
       <form name="contact" method="post">
@@ -19,14 +12,11 @@ function App() {
               Your Name: <input type="text" name="name" />
             </label>
           </p>
-        {/* <ReCAPTCHA 
-          ref={recaptchaRef}
+        <ReCAPTCHA 
           sitekey={process.env.REACT_APP_SITE_KEY} 
-          onChange={onChange}
-        /> */}
+        />
         <button type="submit">Send</button>
       </form>
-      {/* <h1>{formState}</h1> */}
     </div>
   );
 }
