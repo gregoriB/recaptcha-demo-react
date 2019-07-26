f958a5c9d02d80d1df22479ccfd66c30aa374eb6
gregoriB <brandon.gregori@gmail.com>
2019-07-25 19:03:15 -0700

    testing netlify forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ReCAPTCHA from "react-google-recaptcha";
 import './App.css';
 
 function App() {
-  const [recaptchaMessage, setRecaptchaMessage] = useState('');
-  const handleOnChange = () => setRecaptchaMessage('recaptcha worked!!');
+  // const [isRecaptchaGood, setIsRecaptchaGood] = useState(false);
+  // const recaptchaRef = useRef(null);
+  const handleSubmit = e => {
+    e.preventDefault();
+    // console.log(recaptchaRef.current.getValue())
+  }
 
   return (
     <div className="App">
-      <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} onChange={handleOnChange} />
-      <h1>{recaptchaMessage}</h1>
+      <form name="contact" method="post" onSubmit={handleSubmit}>
+        <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+        {/* <ReCAPTCHA 
+          ref={recaptchaRef}
+          sitekey={process.env.REACT_APP_SITE_KEY} 
+          onChange={onChange}
+        /> */}
+        <button type="submit">Send</button>
+      </form>
+      {/* <h1>{formState}</h1> */}
     </div>
   );
 }
