65d5e4dbd52d37b59c7e87ea409835c3e87ff46c
gregoriB <brandon.gregori@gmail.com>
2019-07-25 19:39:43 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReCAPTCHA from "react-google-recaptcha";
 import './App.css';
 
 function App() {
+  const [isCaptchaGood, setIsCaptchaGood] = useState(true);
+
+  const handleChange = () => {
+    setIsCaptchaGood(false);
+  }
+
   return (
     <div className="App">
       <form name="contact" method="post">
         <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-        <ReCAPTCHA 
-          sitekey={process.env.REACT_APP_SITE_KEY} 
-        />
-        <button type="submit">Send</button>
+        <input type="text" name="name" placeholder='Name' required />
+        <input type="email" name="email" placeholder='Your Email' required />
+        <textarea placeholder='Please add your message here' required />
+        <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} onChange={handleChange} />
+        <button type="submit" disabled={isCaptchaGood}>SEND</button>
       </form>
     </div>
   );
