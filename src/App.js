9073a6e26fc03b7b4f394f5ee0fcb76bbbb944ad
gregoriB <brandon.gregori@gmail.com>
2019-07-25 19:45:15 -0700

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <input type="hidden" name="form-name" value="contact" />
         <input type="text" name="name" placeholder='Name' required />
         <input type="email" name="email" placeholder='Your Email' required />
-        <textarea placeholder='Please add your message here' required />
+        <textarea name='message' placeholder='Please add your message here' required />
         <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} onChange={handleChange} />
         <button type="submit" disabled={isCaptchaGood}>SEND</button>
       </form>
